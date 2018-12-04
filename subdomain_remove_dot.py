c32f44cd78153119821fe2c0cdca9c7d7f8e742d
Wareeq Shile <shilewareeq@gmail.com>
2018-12-04 07:31:58 +0100

    upload
    remove . from word list

---
+++ subdomain_remove_dot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from sys import argv
+
+f = open(argv[1],'r')
+n = [x[:-2] for x in f.readlines()]
+n_f = open(argv[1]+'_new.txt','w')
+for z in n:
+    n_f.write(z+"\n")
+n_f.close()
+
+
+    
